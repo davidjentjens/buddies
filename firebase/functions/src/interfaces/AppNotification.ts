05ae92a816490691a9b679f03a9f51570fd7813e
davidjentjens <davidjentjens@gmail.com>
2021-11-15 23:45:47 -0300

    Mudar o campo EVALUATE para EVENT_END no back-end

--- firebase/functions/src/interfaces/AppNotification.ts
+++ firebase/functions/src/interfaces/AppNotification.ts
@@ -4,7 +4,7 @@ export default interface AppNotification{
   id?: string;
   title: string;
   body: string;
-  type: "EVENT_SOON" | "EVALUATE";
+  type: "EVENT_SOON" | "EVENT_END";
   route: string;
   emissionDate: Timestamp;
 }
